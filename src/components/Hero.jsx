be1e60e28634c3bd15458c6a7a33a9945617391f
Md Soroardi Islam <mdsoroardi602828@gmail.com>
2026-01-10 15:22:17 +0600

    hero complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import heroimage from "../assets/hero-image.png"
 
 const Hero = () => {
   return (
@@ -13,14 +14,31 @@ const Hero = () => {
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight">
           We boost the growth for
           <span className="text-blue-600 relative inline-block">
-            
             Startup to Fortune 500
-            <span className="absolute bottom-0 -my-2 left-0 w-full h-0.5 bg-blue-200/60"></span>
-          </span> 
-          Companies <span></span>
+            <span className="absolute bottom-0 -my-2 left-0  w-full h-0.5 bg-blue-200/60"></span>
+          </span>
+          Companies
+          <span className="inline-block ml-2 animate-pulse">⏰</span>
         </h1>
+        <p className="text-gray-600 text-lg md:text-xl max-w-xl">
+          Get the most accurete leads, sales people training and conversions,
+          tools and more-all within the same one billing
+        </p>
+
+        <div className=" flaex gap-3 max-w-md">
+          <input
+            type="email"
+            placeholder="Email address"
+            className="flex-1 px-6 py-2 border border-gray-200 rounded-xl focus:outline-none focus:border-blue-600 focus:ring-2 focus:ring-blue-100 transition-all"
+          />
+          <button className="bg-blue-600 text-white px-6 py-2.5 ml-1 rounded-xl hover:bg-blue-700 cursor-pointer transition-all hover:shadow-lg font-bold hover:shadow-blue-300">→</button>
+        </div>
       </div>
-      <div className="w-full md:w-1/2 ">hghg</div>
+      <div className="w-full md:w-1/2 mt-16 md:mt-0 pl-0 md:pl-12">
+       <div className="relative">
+        <img src={heroimage} alt="hero image" className="rounded-lg relative z-10 hover:scale-[1.02] transition-transform duration-300" />
+       </div>
+      </div>
     </section>
   );
 };
