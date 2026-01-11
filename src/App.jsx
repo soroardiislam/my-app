8db6ea538789b27a3d0c9235542e29715200c4e5
Md Soroardi Islam <mdsoroardi602828@gmail.com>
2026-01-11 15:26:18 +0600

    company-logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
+import CompanyLogo from "./components/CompanyLogo";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main className="relative min-h-screen overflow-x-hidden">
-      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr  from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="absolute -top-28 -left-28 w-125 h-125 bg-linear-to-tr  from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
       <div className="overflow-hidden">
         <Navbar />
         <Hero />
+        <CompanyLogo />
       </div>
     </main>
   );
