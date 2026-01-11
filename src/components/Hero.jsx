dab16c4191bdc94759826728fe483e33d2237600
Md Soroardi Islam <mdsoroardi602828@gmail.com>
2026-01-11 15:35:34 +0600

    some-update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,10 @@ import heroimage from "../assets/hero-image.png";
 
 const Hero = () => {
   return (
-    <section className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-44 pb-6 px-4 sm:px-6 lg:px-8">
+    <section
+      id="home"
+      className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-44 pb-6 px-4 sm:px-6 lg:px-8"
+    >
       <div className="w-full md:w-1/2 space-y-8">
         <div className=" flex items-center gap-2 bg-gray-50 w-fit px-4 py-2 rounded-full hover:bg-gray-100 transition-colors cursor-pointer group">
           <span className="text-blue-600 group-hover:text-amber-300 group-hover:scale-110 transition-transform">
